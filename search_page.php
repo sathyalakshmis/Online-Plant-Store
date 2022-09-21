a099083e01164d900993eb586bf9b3563f3aa1a5
sathyalakshmi s <sathyalakshmis30@gmail.com>
2022-09-21 21:40:33 +0530

    first commit

--- search_page.php
+++ search_page.php
@@ -62,6 +62,7 @@ if(isset($_POST['add_to_cart'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -75,47 +76,48 @@ if(isset($_POST['add_to_cart'])){
    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
-   
-<?php @include 'header.php'; ?>
 
-<section class="heading">
-    <h3>search page</h3>
-    <p> <a href="home.php">home</a> / search </p>
-</section>
+   <?php @include 'header.php'; ?>
 
-<section class="search-form">
-    <form action="" method="POST">
-        <input type="text" class="box" placeholder="search products..." name="search_box">
-        <input type="submit" class="btn" value="search" name="search_btn">
-    </form>
-</section>
+   <section class="heading">
+      <h3>search page</h3>
+      <p> <a href="home.php">home</a> / search </p>
+   </section>
 
-<section class="products" style="padding-top: 0;">
+   <section class="search-form">
+      <form action="" method="POST">
+         <input type="text" class="box" placeholder="search products..." name="search_box">
+         <input type="submit" class="btn" value="search" name="search_btn">
+      </form>
+   </section>
 
-   <div class="box-container">
+   <section class="products" style="padding-top: 0;">
 
-      <?php
+      <div class="box-container">
+
+         <?php
         if(isset($_POST['search_btn'])){
          $search_box = mysqli_real_escape_string($conn, $_POST['search_box']);
          $select_products = mysqli_query($conn, "SELECT * FROM `products` WHERE name LIKE '%{$search_box}%'") or die('query failed');
          if(mysqli_num_rows($select_products) > 0){
             while($fetch_products = mysqli_fetch_assoc($select_products)){
       ?>
-      <form action="" method="POST" class="box">
-         <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
-         <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
-         <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
-         <div class="name"><?php echo $fetch_products['name']; ?></div>
-         <input type="number" name="product_quantity" value="1" min="0" class="qty">
-         <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
-         <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-         <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-         <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-         <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
-         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-      </form>
-      <?php
+         <form action="" method="POST" class="box">
+            <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
+            <div class="price">Rs.<?php echo $fetch_products['price']; ?>/-</div>
+            <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
+            <div class="name"><?php echo $fetch_products['name']; ?></div>
+            <input type="number" name="product_quantity" value="1" min="0" class="qty">
+            <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
+            <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+            <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+            <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+            <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
+            <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+         </form>
+         <?php
          }
             }else{
                 echo '<p class="empty">no result found!</p>';
@@ -125,17 +127,18 @@ if(isset($_POST['add_to_cart'])){
         }
       ?>
 
-   </div>
+      </div>
 
-</section>
-
-
+   </section>
 
 
 
-<?php @include 'footer.php'; ?>
 
-<script src="js/script.js"></script>
+
+   <?php @include 'footer.php'; ?>
+
+   <script src="js/script.js"></script>
 
 </body>
+
 </html>
