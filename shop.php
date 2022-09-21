b8f7c24f2b11cfd438176312fe8a860ff7ed7c5d
sathyalakshmi s <sathyalakshmis30@gmail.com>
2022-09-21 21:18:28 +0530

    first commit

--- shop.php
+++ shop.php
@@ -62,6 +62,7 @@ if(isset($_POST['add_to_cart'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -75,58 +76,60 @@ if(isset($_POST['add_to_cart'])){
    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
-   
-<?php @include 'header.php'; ?>
 
-<section class="heading">
-    <h3>our shop</h3>
-    <p> <a href="home.php">home</a> / shop </p>
-</section>
+   <?php @include 'header.php'; ?>
 
-<section class="products">
+   <section class="heading">
+      <h3>our shop</h3>
+      <p> <a href="home.php">home</a> / shop </p>
+   </section>
 
-   <h1 class="title">latest products</h1>
+   <section class="products">
 
-   <div class="box-container">
+      <h1 class="title">latest products</h1>
 
-      <?php
+      <div class="box-container">
+
+         <?php
          $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
          if(mysqli_num_rows($select_products) > 0){
             while($fetch_products = mysqli_fetch_assoc($select_products)){
       ?>
-      <form action="" method="POST" class="box">
-         <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
-         <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
-         <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
-         <div class="name"><?php echo $fetch_products['name']; ?></div>
-         <input type="number" name="product_quantity" value="1" min="0" class="qty">
-         <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
-         <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-         <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-         <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-         <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
-         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-      </form>
-      <?php
+         <form action="" method="POST" class="box">
+            <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
+            <div class="price">Rs.<?php echo $fetch_products['price']; ?>/-</div>
+            <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
+            <div class="name"><?php echo $fetch_products['name']; ?></div>
+            <input type="number" name="product_quantity" value="1" min="0" class="qty">
+            <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
+            <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+            <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+            <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+            <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
+            <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+         </form>
+         <?php
          }
       }else{
          echo '<p class="empty">no products added yet!</p>';
       }
       ?>
 
-   </div>
+      </div>
 
-</section>
-
-
+   </section>
 
 
 
 
-<?php @include 'footer.php'; ?>
 
-<script src="js/script.js"></script>
+
+   <?php @include 'footer.php'; ?>
+
+   <script src="js/script.js"></script>
 
 </body>
+
 </html>
