3ee3c66aece521f644031b8fe2a6951ffe28ffab
sathyalakshmi s <sathyalakshmis30@gmail.com>
2022-09-21 21:33:01 +0530

    first commit

--- register.php
+++ register.php
@@ -23,7 +23,7 @@ if(isset($_POST['submit'])){
       }else{
          mysqli_query($conn, "INSERT INTO `users`(name, email, password) VALUES('$name', '$email', '$pass')") or die('query failed');
          $message[] = 'registered successfully!';
-         header('location:login.php');
+         header('location:index.php');
       }
    }
 
@@ -33,6 +33,7 @@ if(isset($_POST['submit'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -46,9 +47,10 @@ if(isset($_POST['submit'])){
    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
 
-<?php
+   <?php
 if(isset($message)){
    foreach($message as $message){
       echo '
@@ -60,20 +62,21 @@ if(isset($message)){
    }
 }
 ?>
-   
-<section class="form-container">
 
-   <form action="" method="post">
-      <h3>register now</h3>
-      <input type="text" name="name" class="box" placeholder="enter your username" required>
-      <input type="email" name="email" class="box" placeholder="enter your email" required>
-      <input type="password" name="pass" class="box" placeholder="enter your password" required>
-      <input type="password" name="cpass" class="box" placeholder="confirm your password" required>
-      <input type="submit" class="btn" name="submit" value="register now">
-      <p>already have an account? <a href="login.php">login now</a></p>
-   </form>
+   <section class="form-container">
 
-</section>
+      <form action="" method="post">
+         <h3>register now</h3>
+         <input type="text" name="name" class="box" placeholder="enter your username" required>
+         <input type="email" name="email" class="box" placeholder="enter your email" required>
+         <input type="password" name="pass" class="box" placeholder="enter your password" required>
+         <input type="password" name="cpass" class="box" placeholder="confirm your password" required>
+         <input type="submit" class="btn" name="submit" value="register now">
+         <p>already have an account? <a href="index.php">login now</a></p>
+      </form>
+
+   </section>
 
 </body>
+
 </html>
